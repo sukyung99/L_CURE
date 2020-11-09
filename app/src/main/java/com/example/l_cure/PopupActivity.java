a68955f491692b17bee4bb96f325aeab0d4e57cc
Deokk <deokk1112@gmail.com>
2020-11-09 21:28:17 +0900

    Add PopUp function / PopUp screen

---
+++ app/src/main/java/com/example/l_cure/PopupActivity.java
@@ -0,0 +1,46 @@
+package com.example.lcure;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class PopupActivity extends Activity {
+
+    TextView txtText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.popup_activity);
+
+        //UI 객체생성
+        txtText = (TextView)findViewById(R.id.txtText);
+
+        //데이터 가져오기
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        txtText.setText(data);
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
