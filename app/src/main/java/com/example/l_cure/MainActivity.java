ac44bb982f7ca5cd33fa2deb21490ede48961651
sukyung99 <tnrudsla413@gmail.com>
2020-11-09 20:47:43 +0900

    add Main Screen and first stage screen

---
+++ app/src/main/java/com/example/l_cure/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.l_cure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button setting;
+    Button learn_skills, improving_skills, my_own_words;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        learn_skills = findViewById(R.id.learn_skills);
+        learn_skills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), SelectImprovingSkillsActivity.class);
+//                startActivityForResult(intent,5000);
+            }
+        });
+
+        improving_skills = findViewById(R.id.improving_skills);
+        improving_skills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectImprovingSkillsActivity.class);
+                startActivityForResult(intent,5000);
+            }
+        });
+
+        my_own_words = findViewById(R.id.my_own_words);
+        my_own_words.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), SelectImprovingSkillsActivity.class);
+//                startActivityForResult(intent,5000);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/l_cure/SelectImprovingSkillsActivity.java
@@ -0,0 +1,40 @@
+package com.example.l_cure;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectImprovingSkillsActivity extends AppCompatActivity {
+    Button back, setting;
+    Button syllable_count, word_synthesis, phonemic_fractionation;
+    Button phonemic_synthesis, phonemic_substitution, syllabic_discrimination;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_improving_skills);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                return;
+            }
+        });
+
+        syllable_count = findViewById(R.id.syllable_count);
+        syllable_count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectSyllableCountActivity.class);
+                startActivityForResult(intent,5000);
+            }
+        });
+    }
+}
