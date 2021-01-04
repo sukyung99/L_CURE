60b3191d16f84515d660f59bfb1ac9f3cfd6ef09
boyun <boyoon54@gmail.com>
2021-01-04 22:36:02 +0900

    find keyhash
    This is only for Kakao API.

--- app/src/main/java/com/cnu_helper/l_cure/MainActivity.java
+++ app/src/main/java/com/cnu_helper/l_cure/MainActivity.java
@@ -3,9 +3,16 @@ package com.cnu_helper.l_cure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.security.MessageDigest;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getHashKey(); // 키해시 찾기 (kakao api)
 
         learn_skills = findViewById(R.id.learn_skills);
         learn_skills.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +51,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,5000);
             }
         });
+    }
+
+    private void getHashKey(){
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String key = new String(Base64.encode(md.digest(), 0));
+                Log.d("Hash key:", "!!!!!!!"+key+"!!!!!!"); // print hash key
+            }
+        } catch (Exception e){
+            Log.e("name not found", e.toString());
+        }
     }
 
 }
