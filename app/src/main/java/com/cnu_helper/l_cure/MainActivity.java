ef43d7061dc766dc5b635b1b34721ae827ca8506
boyun <boyoon54@gmail.com>
2021-01-07 21:44:02 +0900

    MyWordStage draft
    just finised main functions, not UI.

--- app/src/main/java/com/cnu_helper/l_cure/MainActivity.java
+++ app/src/main/java/com/cnu_helper/l_cure/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         my_own_words.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SelectMyWordActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SelectMyWordStageActivity.class);
                 startActivityForResult(intent,5000);
             }
         });
--- app/src/main/java/com/cnu_helper/l_cure/SelectMyWordActivity.java
+++ app/src/main/java/com/cnu_helper/l_cure/SelectMyWordActivity.java
@@ -2,46 +2,40 @@ package com.cnu_helper.l_cure;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 public class SelectMyWordActivity extends AppCompatActivity {
-    private List<Words> word_list; // words 리스트
-    private List<Words> new_word_list  = new ArrayList<>();; // 추출한 words 리스트
     private Words word;
     private TextView tv_my_word, tv_writing_word;
-    private Button  btn_speaker, btn_checker;
+    private Button  back, btn_speaker, btn_checker;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_my_word);
 
+        word = (Words) getIntent().getSerializableExtra("my_word");
+
         tv_my_word = (TextView) findViewById(R.id.my_word);
         tv_writing_word = (TextView) findViewById(R.id.writing_word);
         btn_speaker = (Button) findViewById(R.id.speaker);
         btn_checker = (Button) findViewById(R.id.check);
 
-        // 단어 db 로드
-        initLoadDB();
-
-        // 한 글자 단어만 추출
-        for (int i = 0; i < word_list.size(); i++) {
-            if (word_list.get(i).getWord().length() == 1) {
-                new_word_list.add(word_list.get(i));
+        // 뒤로가기 버튼
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                return;
             }
-        }
+        });
 
         // 화면에 word 세팅
-        word = new_word_list.get(0);
         tv_my_word.setText(word.getWord());
         tv_writing_word.setText(word.getWord());
 
@@ -65,21 +59,6 @@ public class SelectMyWordActivity extends AppCompatActivity {
         });
 
     }
-
-    // 데이터베이스 load
-    private void initLoadDB() {
-        DataAdapter mDbHelper = new DataAdapter(this.getApplicationContext());
-        mDbHelper.createDatabase();
-        mDbHelper.open();
-
-        // db에 있는 값들을 model을 적용해서 넣는다.
-        word_list = mDbHelper.getTableData();
-
-        // db 닫기
-        mDbHelper.close();
-    }
-
-
 
 
 }
