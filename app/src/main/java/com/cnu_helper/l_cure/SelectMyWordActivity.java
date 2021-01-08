7a127a9720289b8d343412fbc215d7ae06decb67
boyun <boyoon54@gmail.com>
2021-01-08 19:44:23 +0900

    MyWordStage - not completed save button

--- app/src/main/java/com/cnu_helper/l_cure/SelectMyWordActivity.java
+++ app/src/main/java/com/cnu_helper/l_cure/SelectMyWordActivity.java
@@ -3,15 +3,18 @@ package com.cnu_helper.l_cure;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SelectMyWordActivity extends AppCompatActivity {
+public class SelectMyWordActivity extends AppCompatActivity implements ViewStub.OnClickListener{
     private Words word;
     private TextView tv_my_word, tv_writing_word;
-    private Button  back, btn_speaker, btn_checker;
+    private Button  back, btn_speaker, btn_photo, btn_save;
+
+    private WritingView writingview;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class SelectMyWordActivity extends AppCompatActivity {
         tv_my_word = (TextView) findViewById(R.id.my_word);
         tv_writing_word = (TextView) findViewById(R.id.writing_word);
         btn_speaker = (Button) findViewById(R.id.speaker);
-        btn_checker = (Button) findViewById(R.id.check);
+        btn_photo = (Button) findViewById(R.id.show_photo);
 
         // 뒤로가기 버튼
         back = findViewById(R.id.back);
@@ -40,7 +43,7 @@ public class SelectMyWordActivity extends AppCompatActivity {
         tv_writing_word.setText(word.getWord());
 
         // 이미지 띄우기
-        btn_checker.setOnClickListener(new View.OnClickListener() {
+        btn_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), PopupActivity.class);
@@ -58,7 +61,19 @@ public class SelectMyWordActivity extends AppCompatActivity {
             }
         });
 
+        writingview = (WritingView) findViewById(R.id.writing_view);
+        btn_save = (Button) findViewById(R.id.save);
+        btn_save.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.save:
+                writingview.save(SelectMyWordActivity.this);
+                break;
 
+        }
+    }
 }
--- app/src/main/java/com/cnu_helper/l_cure/WritingView.java
+++ app/src/main/java/com/cnu_helper/l_cure/WritingView.java
@@ -1,14 +1,34 @@
 package com.cnu_helper.l_cure;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WritingView extends View {
 
@@ -32,9 +52,9 @@ public class WritingView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         paint.setColor(Color.BLUE); // 색상
-        paint.setStyle(Paint.Style.STROKE); //STROKE속성을 이용하여 테두리...선...
+        paint.setStyle(Paint.Style.STROKE); // 테두리, 선 등
         paint.setStrokeWidth(50); //두께
-        canvas.drawPath(path, paint); //path객체가 가지고 있는 경로를 화면에 그린다...
+        canvas.drawPath(path, paint); //path 객체가 가지고 있는 경로를 화면에 그린다.
     }
 
     @Override
@@ -57,18 +77,100 @@ public class WritingView extends View {
                 break;
         }
 
-        //View의 onDraw()를 호출하는 메소드...
+        //View의 onDraw()를 호출하는 메소드
         invalidate();
 
         return true;
     }
 
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    // 저장하기
+    @SuppressLint("WrongThread")
+    public void save (Context context) {
+        this.setDrawingCacheEnabled(true);
+
+
+        // 현재 날짜로 파일을 저장하기
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date currentTime = new Date();
+        String dateString = formatter.format(currentTime);
+        String filename = dateString + "_drawImage.png";
+//        try {
+//            File file = new File(Environment.getExternalStorageDirectory(), filename);
+//            if (file.createNewFile())
+//                Log.d("save", "파일 생성 성공");
+//            OutputStream outStream = new FileOutputStream(file);
+//            screenshot.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+//            outStream.close();
 //
-//        DisplayMetrics dm = getResources().getDisplayMetrics();
-//        int width = (int) (200*dm.density);
-//        setMeasuredDimension(width, width);
-//    }
+//            // 갤러리에 변경을 알려줌
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                // 안드로이드 버전이 Kitkat 이상 일때
+//                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//                Uri contentUri = Uri.fromFile(file);
+//                mediaScanIntent.setData(contentUri);
+//                context.sendBroadcast(mediaScanIntent);
+//            } else {
+//                context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
+//            }
+//
+//            Toast.makeText(context.getApplicationContext(), "저장완료", Toast.LENGTH_LONG).show();
+//        } catch (IOException e) {
+//            Toast.makeText(context.getApplicationContext(), "저장 에러", Toast.LENGTH_LONG).show();
+//            e.printStackTrace();
+//        }
+//        this.setDrawingCacheEnabled(false);
+
+//        int writeCheck = ContextCompat.checkSelfPermission(context.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        if(writeCheck != PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(context.getApplicationContext(), "permission error", Toast.LENGTH_LONG).show();
+//        }else {
+//            Toast.makeText(context.getApplicationContext(), "permission ok", Toast.LENGTH_LONG).show();
+//        }
+////////////////////
+        String file_state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(file_state) )
+            Toast.makeText(context.getApplicationContext(), "permission state ok", Toast.LENGTH_LONG).show();
+
+//        File file = new File(Environment.getExternalStorageDirectory().getPath()+filename);
+        OutputStream out = null;
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root);
+        myDir.mkdirs();
+        File file = new File(myDir, filename);
+
+        try{
+            out = new FileOutputStream(file);
+            if(out != null) {
+                Bitmap screenshot = this.getDrawingCache();
+                screenshot.compress(Bitmap.CompressFormat.PNG, 75, out);
+                Toast.makeText(context.getApplicationContext(), "Capture success", Toast.LENGTH_LONG).show();
+            } else Toast.makeText(context.getApplicationContext(), "Capture fail", Toast.LENGTH_LONG).show();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(context.getApplicationContext(), "FileNotFound - error", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(context.getApplicationContext(), "InOut - error", Toast.LENGTH_LONG).show();
+        }
+//////////////////
+//        File storage = context.getCacheDir();
+//        String path = storage.getPath();
+//        File file = new File(storage, filename);
+//        try{
+//            Bitmap screenshot = this.getDrawingCache();
+//            file.createNewFile();
+//            FileOutputStream out = new FileOutputStream(file);
+//            screenshot.compress(Bitmap.CompressFormat.PNG, 75, out);
+//            out.close();
+//            Toast.makeText(context.getApplicationContext(), "save ok: "+path, Toast.LENGTH_LONG).show();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//            Toast.makeText(context.getApplicationContext(), "FileNotFound - error", Toast.LENGTH_LONG).show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Toast.makeText(context.getApplicationContext(), "InOut - error", Toast.LENGTH_LONG).show();
+//        }
+
+    }
 }
